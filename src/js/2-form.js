a326cd8ea35c6932ee58353a243da3d9b0a98e44
oleh-ol <141167685+oleh-ol@users.noreply.github.com>
2024-01-10 20:11:13 +0200

    004

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,29 @@
 'use strict'
 
+const FEEDBACK_STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+let formObject = {};
+
+try {
+  const storageValue = localStorage.getItem(FEEDBACK_STORAGE_KEY);
+  if (storageValue) {
+    formObject = JSON.parse(storageValue);
+    for (let key in formObject) {
+      form.elements[key].value = formObject[key];
+    }
+  }
+} catch (error) {
+  console.error('PARSE ERROR');
+}
+
+form.addEventListener('input', event => {
+  formObject[event.target.name] = event.target.value.trim();
+  localStorage.setItem(FEEDBACK_STORAGE_KEY, JSON.stringify(formObject));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(formObject);
+  localStorage.removeItem(FEEDBACK_STORAGE_KEY);
+  form.reset();
+});
